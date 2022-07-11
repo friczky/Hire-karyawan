13c0d09cb6e8617c798c17f0ba0250969c5b0532
Fadilah Riczky <friczky@gmail.com>
2022-07-11 07:37:55 +0700

    update form tentang

--- Admin/halaman/tentang/index.php
+++ Admin/halaman/tentang/index.php
@@ -17,7 +17,7 @@ include '../../komponen/sidebar.php';
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">User Profile</li>
+              <li class="breadcrumb-item active">Tentang</li>
             </ol>
           </div>
         </div>
