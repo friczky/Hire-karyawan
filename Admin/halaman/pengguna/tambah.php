ddf924b88051bf9ca614b72e313a7e1dbe83bb01
Fadilah Riczky <friczky@gmail.com>
2022-07-11 07:51:27 +0700

    update form tambah pengguna

--- Admin/halaman/pengguna/tambah.php
+++ Admin/halaman/pengguna/tambah.php
@@ -1,5 +1,5 @@
 <?php 
-$judul = 'Tentang';
+$judul = 'Tambah Pengguna';
 include '../../komponen/header.php';
 include '../../komponen/navbar.php';
 include '../../komponen/sidebar.php';
@@ -12,12 +12,12 @@ include '../../komponen/sidebar.php';
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Tentang Aplikasi</h1>
+            <h1>Tambah Penguna</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Tentang</li>
+              <li class="breadcrumb-item active">Tambah Pengguna</li>
             </ol>
           </div>
         </div>
@@ -31,42 +31,44 @@ include '../../komponen/sidebar.php';
           <div class="col-md-12">
             <div class="card">
               <div class="card-header p-2">
-               Form Tentang Aplikasi
+               Form Tambah Penguna
               </div><!-- /.card-header -->
               <div class="card-body">
                 <div class="tab-content">
                   <div class="active tab-pane" id="activity">
 
                   <div class="tab-pane" id="settings">
-                    <form class="form-horizontal">
+                    <form action="" class="form-horizontal">
                       <div class="form-group row">
-                        <label for="inputName" class="col-sm-2 col-form-label">Nama Web</label>
+                        <label for="inputName" class="col-sm-2 col-form-label">Nama</label>
                         <div class="col-sm-10">
-                          <input type="email" class="form-control" id="inputName" placeholder="Name">
+                          <input type="text" name="nama" class="form-control" id="inputName" placeholder="Nama">
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                         <div class="col-sm-10">
-                          <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                          <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Email">
                         </div>
                       </div>
                       <div class="form-group row">
-                        <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
+                        <label for="inputName2" class="col-sm-2 col-form-label">Password</label>
                         <div class="col-sm-10">
-                          <input type="text" class="form-control" id="inputName2" placeholder="Name">
+                          <input type="password" class="form-control" id="inputName2" placeholder="Password">
                         </div>
                       </div>
                       <div class="form-group row">
-                        <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>
+                        <label for="inputExperience" class="col-sm-2 col-form-label">Role</label>
                         <div class="col-sm-10">
-                          <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                          <select name="role" id="" class="form-control">
+                            <option value="">Pilih Role</option>
+                          </select>
                         </div>
                       </div>
                       <div class="form-group row">
-                        <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
+                        <label for="inputExperience" class="col-sm-2 col-form-label">Foto</label>
                         <div class="col-sm-10">
-                          <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
+                          <input type="file" name="foto" id="" class="form-control">
                         </div>
                       </div>
                       
