7456fff57101fdc6a3f895f77b1b12731ec2ba42
Fadilah Riczky <friczky@gmail.com>
2022-07-20 11:47:14 +0700

    :zap: update halaman kontak

--- Home/kontak/index.php
+++ Home/kontak/index.php
@@ -1,248 +1,68 @@
 <?php
 
-include '../komponen/header.php';
+$title = 'Kontak';
+include '../komponen/header-sub.php';
 include '../komponen/navbar.php';
 
 ?>
 
-<div class="page-heading normal-space">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <h6>Author Details</h6>
-          <h2>View Details For Author</h2>
-          <span>Home > <a href="#">Author</a></span>
-          <div class="buttons">
-            <div class="main-button">
-              <a href="explore.html">Explore Our Items</a>
-            </div>
-            <div class="border-button">
-              <a href="create.html">Create Your NFT</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+</div>
 
-  <div class="author-page">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <div class="author">
-            <img src="assets/images/single-author.jpg" alt="" style="border-radius: 50%; max-width: 170px;">
-            <h4>Melanie Smith <br> <a href="#">@melanie32</a></h4>
-          </div>
-        </div>
-        <div class="col-lg-6">
-          <div class="right-info">
-            <div class="row">
-              <div class="col-4">
-                <div class="info-item">
-                  <i class="fa fa-heart"></i>
-                  <h6>1238 <em>Likes</em></h6>
-                </div>
-              </div>
-              <div class="col-4">
-                <div class="info-item">
-                  <i class="fa fa-hand"></i>
-                  <h6>1238 <em>Likes</em></h6>
-                </div>
-              </div>
-              <div class="col-4">
-                <div class="info-item">
-                  <i class="fa fa-dollar"></i>
-                  <h6>1238 <em>Likes</em></h6>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-5">
-                <h5>559 Followers</h5>
-              </div>
-              <div class="col-7">
-                <div class="main-button">
-                  <a href="#">Follow @melanie32</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-12">
-          <div class="section-heading">
-            <div class="line-dec"></div>
-            <h2>Melanie Smith’s <em>Items</em>.</h2>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <div class="item">
-            <div class="row">
-              <div class="col-lg-12">
-                <span class="author">
-                  <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                </span>
-                <img src="assets/images/discover-03.jpg" alt="" style="border-radius: 20px;">
-                <h4>Mutant Ape Bored</h4>
-              </div>
-              <div class="col-lg-12">
-                <div class="line-dec"></div>
-                <div class="row">
-                  <div class="col-6">
-                    <span>Current Bid: <br> <strong>2.03 ETH</strong></span>
-                  </div>
-                  <div class="col-6">
-                    <span>Ends In: <br> <strong>25th Nov</strong></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="main-button">
-                  <a href="details.html">View Details</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <div class="item">
-            <div class="row">
-              <div class="col-lg-12">
-                <span class="author">
-                  <img src="assets/images/author-02.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                </span>
-                <img src="assets/images/discover-06.jpg" alt="" style="border-radius: 20px;">
-                <h4>Mutant Ape Bored</h4>
-              </div>
-              <div class="col-lg-12">
-                <div class="line-dec"></div>
-                <div class="row">
-                  <div class="col-6">
-                    <span>Current Bid: <br> <strong>2.03 ETH</strong></span>
-                  </div>
-                  <div class="col-6">
-                    <span>Ends In: <br> <strong>25th Nov</strong></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="main-button">
-                  <a href="details.html">View Details</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <div class="item">
-            <div class="row">
-              <div class="col-lg-12">
-                <span class="author">
-                  <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                </span>
-                <img src="assets/images/discover-05.jpg" alt="" style="border-radius: 20px;">
-                <h4>Mutant Ape Bored</h4>
-              </div>
-              <div class="col-lg-12">
-                <div class="line-dec"></div>
-                <div class="row">
-                  <div class="col-6">
-                    <span>Current Bid: <br> <strong>2.03 ETH</strong></span>
-                  </div>
-                  <div class="col-6">
-                    <span>Ends In: <br> <strong>25th Nov</strong></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="main-button">
-                  <a href="details.html">View Details</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <div class="item">
-            <div class="row">
-              <div class="col-lg-12">
-                <span class="author">
-                  <img src="assets/images/author-03.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                </span>
-                <img src="assets/images/discover-04.jpg" alt="" style="border-radius: 20px;">
-                <h4>Mutant Ape Bored</h4>
-              </div>
-              <div class="col-lg-12">
-                <div class="line-dec"></div>
-                <div class="row">
-                  <div class="col-6">
-                    <span>Current Bid: <br> <strong>2.03 ETH</strong></span>
-                  </div>
-                  <div class="col-6">
-                    <span>Ends In: <br> <strong>25th Nov</strong></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="main-button">
-                  <a href="details.html">View Details</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <!-- about section -->
 
-  <div class="create-nft">
+  <section class="about_section layout_padding">
     <div class="container">
       <div class="row">
-        <div class="col-lg-8">
-          <div class="section-heading">
-            <div class="line-dec"></div>
-            <h2>Create Your NFT & Put It On The Market.</h2>
+        <div class="col-md-6">
+          <div class="detail-box">
+            <div class="heading_container">
+              <h2>
+                KONTAK <?= $data_tentang['nama_web']?>
+              </h2>
+            </div>
+            <p>
+              <ul>
+                <li><i class="fa fa-at"></i> Email : <?= $data_kontak['email']?></li>
+                <li><i class="fa fa-phone"></i> Telpon : <?= $data_kontak['telpon']?></li>
+                <li><i class="fa fa-map-marker"></i> Alamat : <?= $data_kontak['alamat']?></li>
+
+              </ul>
+            </p>
+            <a href="<?= login()?>">
+              <i class="fa fa-whatsapp"></i> Whatsapp
+            </a>
           </div>
         </div>
-        <div class="col-lg-4">
-          <div class="main-button">
-            <a href="create.html">Create Your NFT Now</a>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="item first-item">
-            <div class="number">
-              <h6>1</h6>
-            </div>
-            <div class="icon">
-              <img src="assets/images/icon-02.png" alt="">
-            </div>
-            <h4>Set Up Your Wallet</h4>
-            <p>Lorem ipsum dolor sit amet, consectetu dipiscingei elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="item second-item">
-            <div class="number">
-              <h6>2</h6>
-            </div>
-            <div class="icon">
-              <img src="assets/images/icon-04.png" alt="">
-            </div>
-            <h4>Add Your Digital NFT</h4>
-            <p>Lorem ipsum dolor sit amet, consectetu dipiscingei elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="item">
-            <div class="icon">
-              <img src="assets/images/icon-06.png" alt="">
-            </div>
-            <h4>Sell Your NFT &amp; Make Profit</h4>
-            <p>Lorem ipsum dolor sit amet, consectetu dipiscingei elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div class="col-md-6">
+          <div class="img-box">
+            <img src="<?= folder_upload()?><?= $data_tentang['logo']?>" alt="">
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
+
+  <!-- end about section -->
+    <hr>
+  <!-- maps section  -->
+    <section class="container-fluid">
+    <div class="" >
+        <?php
+        $data = $data_tentang['maps_url'];
+        $replace = str_replace('width="800" height="600"', 'width="100%" height="600"', $data);
+        echo $replace;
+        ?>
+    </div>
+    </section>
+  <!-- end maps section  -->
+
+  <!-- info section -->
+  <section class="info_section ">
+    <div class="container">
+
+      </div>
+    </div>
+  </section>
+  <!-- end info_section -->
 
 <?php include '../komponen/footer.php'?>
